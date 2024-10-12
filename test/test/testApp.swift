2d066ec8188d0b228134030bc116deba10add005
Mariah <mariahgardner00@gmail.com>
2024-10-12 14:51:33 -0500

    Inital commit

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Mariah Jade Gardner on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
